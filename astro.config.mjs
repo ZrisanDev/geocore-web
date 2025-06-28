66e848dd6b741dc7df01e48ea1996c4a046e7dcb
ZrisanDev <103600178+ZrisanDev@users.noreply.github.com>
2025-06-28 18:45:08 -0500

    agregnaod netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+  output: "static",
+
+  adapter: netlify(),
 });
